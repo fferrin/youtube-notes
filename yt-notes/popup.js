39800288c9599e3f4b4decffcd182feea2c1bb04
Facundo Ferrin <facundo.ferrin@gmail.com>
2024-02-23 22:00:38 -0800

    Fix saving channel Alias and videos

--- yt-notes/popup.js
+++ yt-notes/popup.js
@@ -96,7 +96,7 @@ document.addEventListener("videoInfoUpdated", function (payload) {
   chrome.storage.local.get({ [videoId]: {} }, function (result) {
     const notesInVideo = result[videoId] ?? {};
     const note = document.getElementById("ytNote");
-    note.value = notesInVideo[timestamp] ?? ""
+    note.value = notesInVideo[timestamp] ?? "";
   });
 });
 
@@ -161,6 +161,9 @@ document.addEventListener("videoInfoUpdated", function (payload) {
     .addEventListener("click", function () {
       const note = document.getElementById("ytNote").value;
       const timestamp = document.getElementById("ytCurrentTime").innerHTML;
+      const channelAlias = document.getElementById("ytChannelAlias").innerHTML;
+      const channelName = document.getElementById("ytChannelName").innerHTML;
+      const videoTitle = document.getElementById("ytTitle").innerHTML;
       const notesInVideo = JSON.parse(window.localStorage.getItem("notes"));
       document.dispatchEvent(
         new CustomEvent("noteSaved", {
@@ -168,6 +171,9 @@ document.addEventListener("videoInfoUpdated", function (payload) {
             videoId: data.videoId,
             note,
             timestamp,
+            channelAlias,
+            channelName,
+            videoTitle,
             notesInVideo,
           },
         })
@@ -192,280 +198,63 @@ document.addEventListener("DOMContentLoaded", async function () {
   chrome.tabs.sendMessage(activeTab.id, { type: "requestVideoInfo" });
 });
 
-/////////
+document.addEventListener("noteSaved", function (payload) {
+  chrome.storage.local.get([payload.detail.channelAlias], function (result) {
+    const videosInChannel = result[payload.detail.channelAlias] ?? {};
+    videosInChannel[payload.detail.videoId] = payload.detail.videoTitle;
+    chrome.storage.local.set({
+      [payload.detail.channelAlias]: videosInChannel,
+    });
+  });
+});
 
-// async function updateNotes(videoId) {
-//   chrome.storage.local.get({ [videoId]: {} }, function (result) {
-//     const notesInVideo = result[videoId] ?? {};
-//
-//     var ulElement = document.getElementById("ytNotes");
-//     ulElement.innerHTML = "";
-//
-//     for (var timestamp in notesInVideo) {
-//       const li = document.createElement("li");
-//       const a = document.createElement("a");
-//       const textBetween = document.createElement("span");
-//       const deleteButton = document.createElement("button");
-//
-//       a.href = "#";
-//       a.textContent = formatTime(timestamp);
-//
-//       textBetween.textContent = notesInVideo[timestamp];
-//
-//       // Delete button
-//       deleteButton.textContent = "Delete";
-//
-//       (function (timestamp) {
-//         deleteButton.addEventListener("click", function () {
-//           li.remove();
-//           const event = new CustomEvent("noteDeleted", {
-//             detail: { timestamp },
-//           });
-//           document.dispatchEvent(event);
-//         });
-//       })(timestamp);
-//
-//       (function (timestamp) {
-//         a.addEventListener("click", function () {
-//           const event = new CustomEvent("noteEdited", {
-//             detail: { timestamp, note: notesInVideo[timestamp] },
-//           });
-//           document.dispatchEvent(event);
-//         });
-//       })(timestamp);
-//
-//       li.appendChild(a);
-//       li.appendChild(textBetween);
-//       li.appendChild(deleteButton);
-//
-//       ulElement.appendChild(li);
-//     }
-//   });
-// }
+document.addEventListener("noteSaved", function (payload) {
+  chrome.storage.local.get(["channels"], function (result) {
+    const channels = result["channels"] ?? {};
+    if (!(payload.detail.channelAlias in channels)) {
+      channels[payload.detail.channelAlias] = payload.detail.channelName;
+      chrome.storage.local.set({ channels: channels });
+    }
+  });
+});
 
-// async function removeNoteFromStorage(videoId, timestamp) {
-//   const notes = await chrome.storage.local.get([videoId]);
-//   delete notes[videoId][timestamp];
-//   await chrome.storage.local.set({ [videoId]: notes[videoId] });
-// }
-//
-// function seekTo(seconds) {
-//   // document.getElementById("movie_player").seekTo(seconds);
-//   document.getElementsByTagName("video")[0].currentTime = seconds;
-// }
+document.addEventListener("noteDeleted", function (payload) {
+  chrome.storage.local.get([payload.detail.videoId], function (result) {
+    const notesInVideo = result[payload.detail.videoId] ?? {};
+    delete notesInVideo[payload.detail.timestamp];
 
-// document.addEventListener("videoInfoUpdated", function (payload) {
-//   chrome.storage.local.get({ [payload.detail.videoId]: {} }, function (result) {
-//     const notesInVideo = result[payload.detail.videoId] ?? {};
-//     console.info("NOTES IN VIDEO", notesInVideo);
-//
-//     var ulElement = document.getElementById("ytNotes");
-//     ulElement.innerHTML = "";
-//
-//     for (var timestamp in notesInVideo) {
-//       const li = document.createElement("li");
-//       const a = document.createElement("a");
-//       const textBetween = document.createElement("span");
-//       const deleteButton = document.createElement("button");
-//
-//       a.href = "#";
-//       a.textContent = formatTime(timestamp);
-//
-//       textBetween.textContent = notesInVideo[timestamp];
-//
-//       // Delete button
-//       deleteButton.textContent = "Delete";
-//       (function (timestamp) {
-//         deleteButton.addEventListener("click", function () {
-//           console.log(`Delete clicked for: ${timestamp}`);
-//           removeNoteFromStorage(videoId, timestamp);
-//           li.remove();
-//         });
-//       })(timestamp);
-//
-//       (function (timestamp) {
-//         a.addEventListener("click", function (e) {
-//           // event.preventDefault();
-//           const event = new CustomEvent("noteEdited", {
-//             detail: { timestamp, note: notesInVideo[timestamp] },
-//           });
-//           document.dispatchEvent(event);
-//
-//           // chrome.tabs.query(
-//           //   { active: true, currentWindow: true },
-//           //   function (tabs) {
-//           //     var currentTab = tabs[0];
-//           //
-//           //     chrome.scripting.executeScript({
-//           //       target: { tabId: currentTab.id },
-//           //       func: seekTo,
-//           //       args: [timestamp],
-//           //     });
-//           //   }
-//           // );
-//           // note.value = notesInVideo[timestamp];
-//         });
-//       })(timestamp);
-//
-//       li.appendChild(a);
-//       li.appendChild(textBetween);
-//       li.appendChild(deleteButton);
-//
-//       ulElement.appendChild(li);
-//     }
-//   });
-// });
+    if (Object.entries(notesInVideo).length === 0) {
+      chrome.storage.local.remove([payload.detail.videoId]);
+    }
+  });
+});
 
-// document.addEventListener("DOMContentLoaded", async function () {
-//   // (async () => {
-//   //   console.error("SALUDE AL CONTENT");
-//   //   const response = await chrome.runtime.sendMessage({ greeting: "hello" });
-//   //   // do something with response here, not outside the function
-//   //   console.log(response);
-//   // })();
-//   const saveButton = document.getElementById("ytSaveButton");
-//   const note = document.getElementById("ytNote");
-//
-//   const title = window.localStorage.getItem("ytTitle");
-//   const url = window.localStorage.getItem("ytVideoUrl");
-//   const channelName = window.localStorage.getItem("ytChannelName");
-//   const channelAlias = window.localStorage.getItem("ytChannelAlias");
-//   const videoId = window.localStorage.getItem("ytVideoId");
-//   const currentTime = window.localStorage.getItem("ytCurrentTime");
-//   const imgSrc = window.localStorage.getItem("ytImgSrc");
-//
-//   console.warn("SETTING UP");
-//   console.warn({ title, url, channelName, channelAlias, videoId, currentTime });
-//   document.getElementById("ytTitle").innerHTML = title;
-//   document.getElementById("ytUrl").innerHTML = url;
-//   document.getElementById("ytChannelName").innerHTML = channelName;
-//   document.getElementById("ytChannelAlias").innerHTML = channelAlias;
-//   document.getElementById("ytVideoId").innerHTML = videoId;
-//   document.getElementById("ytCurrentTime").innerHTML = currentTime;
-//   document.getElementById("ytImg").src = imgSrc;
-//
-//   const videoIdText = window.localStorage.getItem("ytVideoId");
-//   function dispatchNotesUpdatedEvent(videoIdParam) {
-//     const event = new CustomEvent("notesUpdated", { detail: { videoIdParam } });
-//     document.dispatchEvent(event);
-//   }
-//
-//   // Function to handle the custom event and update the content of the "escucha" div
-//   function handleNotesUpdatedEvent(event) {
-//     const notes = document.getElementById("ytNotes2");
-//     const videoIdParam = event.detail.videoIdParam;
-//     notes.textContent =
-//       'Event "createdAhora" received at ' +
-//       new Date().toLocaleTimeString() +
-//       ". VideoID: " +
-//       videoIdParam;
-//   }
-//
-//   // Add event listeners
-//   // document.getElementById("ytNotes2").addEventListener("notesUpdated", handleNotesUpdatedEvent);
-//   document.addEventListener("notesUpdated", handleNotesUpdatedEvent);
-//
-//   // TODO: Ver esto
-//   // chrome.storage.local.get([videoIdText], function (result) {
-//   chrome.storage.local.get({ [videoIdText]: {} }, function (result) {
-//     const notesInVideo = result[videoIdText] ?? {};
-//
-//     var ulElement = document.getElementById("ytNotes");
-//     ulElement.innerHTML = "";
-//
-//     for (var timestamp in notesInVideo) {
-//       const li = document.createElement("li");
-//       const a = document.createElement("a");
-//       const textBetween = document.createElement("span");
-//       const deleteButton = document.createElement("button");
-//
-//       a.href = "#";
-//       a.textContent = formatTime(timestamp);
-//
-//       textBetween.textContent = notesInVideo[timestamp];
-//
-//       // Delete button
-//       deleteButton.textContent = "Delete";
-//       (function (timestamp) {
-//         deleteButton.addEventListener("click", function () {
-//           console.log(`Delete clicked for: ${timestamp}`);
-//           removeNoteFromStorage(videoIdText, timestamp);
-//           li.remove();
-//         });
-//       })(timestamp);
-//
-//       (function (timestamp) {
-//         a.addEventListener("click", function (event) {
-//           event.preventDefault();
-//
-//           chrome.tabs.query(
-//             { active: true, currentWindow: true },
-//             function (tabs) {
-//               var currentTab = tabs[0];
-//
-//               chrome.scripting.executeScript({
-//                 target: { tabId: currentTab.id },
-//                 func: seekTo,
-//                 args: [timestamp],
-//               });
-//             }
-//           );
-//           note.value = notesInVideo[timestamp];
-//         });
-//       })(timestamp);
-//
-//       li.appendChild(a);
-//       li.appendChild(textBetween);
-//       li.appendChild(deleteButton);
-//
-//       ulElement.appendChild(li);
-//     }
-//   });
-//
-//   // TODO: Ver esto
-//   // chrome.storage.local.get([videoIdText], function (result) {
-//   chrome.storage.local.get({ [videoIdText]: {} }, function (result) {
-//     const timee = window.localStorage.getItem("ytCurrentTime");
-//     note.value = result[videoIdText][timee] ?? "";
-//     // console.error(document.getelementbyid("ytcurrenttime").innertext);
-//     // const notesinvideo = result[videoidtext] ?? {};
-//     // notesinvideo[currenttimetext] = note.value;
-//     // chrome.storage.local.set({
-//     //   [videoidtext]: notesinvideo,
-//     // });
-//   });
-//
-//   saveButton.addEventListener("click", function () {
-//     const channelAliasText = channelAlias.innerText;
-//     const channelNameText = channelName.innerText;
-//     const videoIdText = videoId.innerText;
-//     const currentTimeText = currentTime.innerText;
-//
-//     // Save channelAlias in channels
-//     chrome.storage.local.get(["channels"], function (result) {
-//       const channels = result.channels ?? {};
-//       channels[channelAliasText] = channelNameText;
-//       chrome.storage.local.set({ channels });
-//     });
-//
-//     // Save videoId in channel alias
-//     chrome.storage.local.get([channelAliasText], function (result) {
-//       const videosInChannel = result[channelAliasText] ?? {};
-//       videosInChannel[videoId.innerText] =
-//         window.localStorage.getItem("ytTitle");
-//       chrome.storage.local.set({ [channelAliasText]: videosInChannel });
-//     });
-//
-//     // Save currentTime in videoId
-//     chrome.storage.local.get([videoIdText], function (result) {
-//       const notesInVideo = result[videoIdText] ?? {};
-//       notesInVideo[currentTimeText] = note.value;
-//       chrome.storage.local.set({
-//         [videoIdText]: notesInVideo,
-//       });
-//       updateNotes(videoIdText);
-//     });
-//     chrome.runtime.sendMessage({ action: "noteUpdated" });
-//   });
-// });
-//
+document.addEventListener("noteDeleted", function (payload) {
+  chrome.storage.local.get([payload.detail.videoId], function (result) {
+    const notesInVideo = result[payload.detail.videoId] ?? {};
+    delete notesInVideo[payload.detail.timestamp];
+
+    if (Object.entries(notesInVideo).length === 0) {
+      chrome.storage.local.remove([payload.detail.videoId]);
+
+      // TODO: Not working. ChannelAlias and Channel in videos are not removed
+      const channelAlias = document.getElementById("ytChannelAlias").innerHTML;
+      chrome.storage.local.get([channelAlias], function (result) {
+        const videosInChannel = result[channelAlias] ?? {};
+        delete videosInChannel[payload.detail.videoId];
+
+        if (Object.entries(videosInChannel).length == 0) {
+          chrome.storage.local.remove([channelAlias]);
+
+          chrome.storage.local.get(["channels"], function (result) {
+            const channels = result["channels"] ?? {};
+            delete channels[channelAlias];
+            chrome.storage.local.set({ channels });
+          });
+        } else {
+          chrome.storage.local.set({ [channelAlias]: videosInChannel });
+        }
+      });
+    }
+  });
+});
