033fe532e5929d0d765ecb5c6fafbb6db57f6b37
Facundo Ferrin <facundo.ferrin@gmail.com>
2026-05-16 01:22:04 +0200

    Add new screenshots

---
+++ images/script.bash
@@ -0,0 +1,5 @@
+convert -background none -size 16x16 logo.svg icon-16.png
+convert -background none -size 32x32 logo.svg icon-32.png
+convert -background none -size 48x48 logo.svg icon-48.png
+convert -background none -size 128x128 logo.svg icon-128.png
+
